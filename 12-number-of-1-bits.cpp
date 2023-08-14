93d4c4a95816a3caf91e369410dd64a8c0cd196a
kushal <gkushalg01@gmail.com>
2023-08-15 02:38:28 +0530

    Beats 100% speed and 70% memory

---
+++ 12-number-of-1-bits.cpp
@@ -0,0 +1,53 @@
+// DSA.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+// beats 100% speed and 25% memory
+int hammingWeight(uint32_t n) {
+    int ans = 0;
+    while (n) {
+        if (n & 1) ++ans;
+        n >>= 1;
+    }
+    return ans;
+
+}
+
+// beats 32% speed(4ms) and 71% memory
+int hammingWeight2(uint32_t n) {
+    int ans = 0;
+    while (n) {
+        if (n & 1) ++ans;
+        n >>= 1;
+    }
+    return ans;
+
+}
+
+/*
+int main()
+{
+    auto s = std::chrono::high_resolution_clock::now();
+    int a = 10, b = 20;
+    std::cout << hammingWeight(a);
+    std::cout << hammingWeight2(b);
+    auto e = std::chrono::high_resolution_clock::now();
+    auto t = std::chrono::duration < double, std::milli>(e - s).count();
+    std::cout << "Time = " << t << "\n";
+}
+*/
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
